779c393cd96ed09c67651e9363f2028bafeb786b
vanshmodi1 <144938453+vanshmodi1@users.noreply.github.com>
2026-07-07 22:34:17 +0530

    Time: 19 ms (35.68%) | Memory: 108.1 MB (10.54%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        //vansh
+        Set<Integer> entry = new HashSet<>();
+
+        for(int i=0;i<nums.length;i++){
+            if(entry.contains(nums[i])){
+                return true;
+            }
+
+            entry.add(nums[i]);
+        }
+        
+        return false;
+    }
+}
